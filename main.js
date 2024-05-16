eadf5e300aa363edca63268756226c21bc4d5bd1
tahaawan123 <tahaawan598@gmail.com>
2024-05-17 02:25:03 +0500

    Anonymous function

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+// 4.Anonymous functions:
+// • Assign a function expression to a variable, with one parameter that outputs the provided 
+// argument to the console.
+// • Pass an argument into the function.
+// • Create the same function as a normal function declaration.
+let anonymousFun = function (str) {
+    console.log(str);
+};
+// • Pass an argument into the function.
+anonymousFun("Hello World");
+// • Create the same function as a normal function declaration.
+function simpleFun(str) {
+    console.log(str);
+}
+simpleFun("Hello! Taha");
